0f2b45dcd960db72254774eb8427a129bea94aca
Nasseryoyo <youussefnasser@gmail.com>
2024-09-26 21:12:11 +0300

    double checking the branch name

--- Backend/src/Services/filter.service.ts
+++ Backend/src/Services/filter.service.ts
@@ -3,8 +3,4 @@ import { Document } from "mongoose";
 
 const filter = async (query: string, model: Document) => {
     const filter = JSON.parse(query);
-    const data = await model
-        .find(filter)
-        .exec();
-    return data;
 }
