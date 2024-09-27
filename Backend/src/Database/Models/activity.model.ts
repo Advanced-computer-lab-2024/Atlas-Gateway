aa43fc6a0b6a879b229c3a3add23a121aec5ec78
Mohamad Rageh <rageh205@gmail.com>
2024-09-27 20:05:58 +0300

    feat/CRUD for Activity Model

--- Backend/src/Database/Models/activity.model.ts
+++ Backend/src/Database/Models/activity.model.ts
@@ -1,23 +1,33 @@
-import { Document, Schema, Types, model } from "mongoose";
+import { Document, Schema, model } from "mongoose";
 
 export interface IActivity extends Document {
-  name: string;
   description: string;
-  category: string;
-  createdBy: Types.ObjectId;
-  // status: 'active' | 'inactive' | 'deleted';
+  dateTime: Date;
+  location: string;
+  tags: string; // could be modified later based on a question (2 and 3)
+  category: string; // will be modified later based on the schema of activity category
+  minPrice: number;
+  maxPrice: number;
+  specialDiscounts: number;
+  isOpen: boolean;
   createdAt: Date;
   updatedAt: Date;
 }
 
-const activitySchema = new Schema<IActivity>({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User'},
-  // status: { type: String, required: true, enum: ['active', 'inactive', 'deleted'] },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-});
-
-export const Activity = model<IActivity>('Activity', activitySchema);
+const activitySchema = new Schema<IActivity>(
+  {
+    description: { type: String, required: true },
+    dateTime: { type: Date, required: true },
+    location: { type: String, required: true },
+    tags: { type: String, required: true },
+    category: { type: String, required: true },
+    minPrice: { type: Number, required: true, min: 0 }, // more validation to be done for example minPrice > maxPrice
+    maxPrice: { type: Number, required: true, min: 0 },
+    specialDiscounts: { type: Number, required: true, min: 0, max: 100 },
+    isOpen: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const Activity = model<IActivity>("Activity", activitySchema);
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,18 +1,17 @@
-import express from 'express';
-import { SERVER } from './Config/config';
-import connectDB from './Database/db';
-import { Types } from 'mongoose';
-import { Activity } from './Database/Models/activity.model';
+import express from "express";
+import { SERVER } from "./Config/config";
+import connectDB from "./Database/db";
+import { Types } from "mongoose";
+import activityRouter from "./Routes/activity.route";
 
 const app = express();
 
-
 async function startServer() {
-
-    app.use(express.json());
-    app.listen(SERVER.port, () => {
-        console.log(`Server is running on http://${SERVER.host}:${SERVER.port}`);
-    });
+  app.use(express.json());
+  app.use("/activity", activityRouter);
+  app.listen(SERVER.port, () => {
+    console.log(`Server is running on http://${SERVER.host}:${SERVER.port}`);
+  });
 }
 
 connectDB();
