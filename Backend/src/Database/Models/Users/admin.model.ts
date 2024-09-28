512ec17277d3944bd730025ca3ab15aed05aaf98
itsXrgon <ali.koheil@gmail.com>
2024-09-28 23:29:54 +0300

    undo pretteir changes

--- Backend/src/Database/Models/Users/admin.model.ts
+++ Backend/src/Database/Models/Users/admin.model.ts
@@ -1,23 +1,21 @@
-import { Document, Types, Schema, model } from 'mongoose';
-import { schemaConfig } from '@utils';
+import { schemaConfig } from "@utils";
+import { Document, Types, Schema, model } from "mongoose";
+
 
 export interface IAdmin extends Document {
-	id: Types.ObjectId;
-	userName: string;
-	email: string; // story 3
-	password: string;
-	isDeleted: boolean;
+    id : Types.ObjectId;
+    userName: string; 
+    email: string; // story 3
+    password: string;
+    isDeleted: boolean;
 }
 
-const adminSchema = new Schema<IAdmin>(
-	{
-		id: { type: Schema.Types.ObjectId, required: true },
-		userName: { type: String, required: true },
-		email: { type: String, required: true },
-		password: { type: String, required: true },
-		isDeleted: { type: Boolean },
-	},
-	schemaConfig
-);
+const adminSchema = new Schema<IAdmin>({
+    id: { type: Schema.Types.ObjectId, required: true },
+    userName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    isDeleted: { type: Boolean },
+}, schemaConfig);
 
 export const Admin = model<IAdmin>('Admin', adminSchema);
