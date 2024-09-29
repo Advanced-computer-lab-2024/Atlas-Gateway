1e3656e26f0437296b2f3048c1f4f2863d696a3b
Omar_Riad <omarriad2006@gmail.com>
2024-09-29 22:20:30 +0300

    rebase: fix/product-CRUD onto fc2045f

--- Backend/src/Routes/product.route.ts
+++ Backend/src/Routes/product.route.ts
@@ -7,5 +7,6 @@ const productRouter = Router();
 productRouter.post("/create", product.createProduct);
 productRouter.get("/list", product.getProducts);
 productRouter.get("/get/:id", product.getProduct);
+productRouter.put("/update/:id", product.updateProduct);
 
 export default productRouter;
