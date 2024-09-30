89535bd2fb007ed745f03e5785d6f2dc5b26fd5c
Omar_Riad <omarriad2006@gmail.com>
2024-10-01 00:48:24 +0300

    feat: Pref-CR & Hist-CR

---
+++ Backend/src/Database/Models/tag.model.ts
@@ -0,0 +1,21 @@
+import { Document, ObjectId, Schema, Types, model } from "mongoose";
+import { schemaConfig } from "#utils";
+
+interface ITag extends Document {
+  name: string;
+  //description: string;
+  type:  string;
+  //createdBy: ObjectId;
+
+}
+
+const tagSchema = new Schema<ITag>({
+  name: { type: String, required: true },
+  //description: { type: String, required: true },
+  type: { type: String, required: true,},
+  //createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Why is this needed?
+  
+}, 
+schemaConfig);
+
+export const Tag = model<ITag>('Tag', tagSchema);
--- Backend/src/Routes/tag.route.ts
+++ Backend/src/Routes/tag.route.ts
@@ -1,3 +1,9 @@
 // Pref-Tags and Historical tags will be placed in one controller, makes sense?
 import { Router } from "express";
 import * as tagController from "../Controllers/tag.controller"
+
+const tagRouter = Router();
+
+
+
+export default tagRouter;
