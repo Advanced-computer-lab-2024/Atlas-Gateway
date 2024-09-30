1c66723b6a2cdc8a939070d0ff6a4286d558e924
Mohamad Rageh <rageh205@gmail.com>
2024-09-30 20:27:03 +0300

    feat: adjusted governor model + formated the code using pretty

--- Backend/src/Controllers/Users/governor.controller.ts
+++ Backend/src/Controllers/Users/governor.controller.ts
@@ -5,14 +5,13 @@ import { Governor } from "../../Database/Models/Users/governor.model";
 export const crteatGovernor = async (req: Request, res: Response) => {
 	try {
 		const { userName, email, password, historicalLocations } = req.body;
-		// if(!userName || !email || !password || !historicalLocations){
-		//   res.status(400).send("userName, email, password and historicalLocations are required");
-		// }
+		if (!userName || !email || !password) {
+			res.status(400).send("userName, email and password are required");
+		}
 		const governor = await Governor.create({
 			userName,
 			email,
 			password,
-			historicalLocations,
 		});
 		res.status(201).send(governor);
 	} catch (error) {
--- Backend/src/Database/Models/museumTag.model.ts
+++ Backend/src/Database/Models/museumTag.model.ts
@@ -2,6 +2,4 @@ import { schemaConfig } from "#utils";
 import { Document, Schema, Types, model } from "mongoose";
 
 // These tags will be basically a key-value pair, since the name of the tag is undefined and the value is also undefined
-interface IMuseumTag extends Document{
-
-}
+interface IMuseumTag extends Document {}
