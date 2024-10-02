4eb3489e23e4375561c70b24c48a35ef7a4d23e8
Mohamad Rageh <rageh205@gmail.com>
2024-10-03 00:32:25 +0300

    feat: adding delete route for admin

--- Backend/src/Routes/Users/admin.route.ts
+++ Backend/src/Routes/Users/admin.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import {
 	createAdmin,
+	deleteAdmin,
 	getAdmins,
 } from "../../Controllers/Users/admin.controller";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.post("/create", createAdmin);
 router.get("/list", getAdmins);
+router.delete("/delete/:id", deleteAdmin);
 
 export default router;
