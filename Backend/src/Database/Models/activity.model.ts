34ab165e875291528963a2b5e9d1873e8da9904f
Mohamad Rageh <rageh205@gmail.com>
2024-10-02 08:02:29 +0300

    feat: updating activity model and controller

--- Backend/src/Database/Models/activity.model.ts
+++ Backend/src/Database/Models/activity.model.ts
@@ -5,14 +5,12 @@ export interface IActivity extends Document {
 	description: string;
 	dateTime: Date;
 	location: string;
-	tags: string; // could be modified later based on a question (2 and 3)
-	category: string; // will be modified later based on the schema of activity category
+	tags: Schema.Types.ObjectId[];
+	category: Schema.Types.ObjectId;
 	minPrice: number;
 	maxPrice: number;
 	specialDiscounts: number;
 	isOpen: boolean;
-	createdAt: Date;
-	updatedAt: Date;
 }
 
 const activitySchema = new Schema<IActivity>(
@@ -20,8 +18,12 @@ const activitySchema = new Schema<IActivity>(
 		description: { type: String, required: true },
 		dateTime: { type: Date, required: true },
 		location: { type: String, required: true },
-		tags: { type: String, required: true },
-		category: { type: String, required: true },
+		tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+		category: {
+			type: Schema.Types.ObjectId,
+			ref: "Category",
+			required: true,
+		},
 		minPrice: { type: Number, required: true, min: 0 },
 		maxPrice: {
 			type: Number,
@@ -31,7 +33,7 @@ const activitySchema = new Schema<IActivity>(
 				validator: function (value) {
 					return value >= this.minPrice;
 				},
-				message: "max price cant be smaller than min price",
+				message: "Max price can't be smaller than min price",
 			},
 		},
 		specialDiscounts: { type: Number, required: true, min: 0, max: 100 },
@@ -39,4 +41,5 @@ const activitySchema = new Schema<IActivity>(
 	},
 	schemaConfig,
 );
+
 export const Activity = model<IActivity>("Activity", activitySchema);
