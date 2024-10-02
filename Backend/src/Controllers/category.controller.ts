e4a3bf377f650c83b7dd2280195e37f6f9a8a95c
Mohamad Rageh <rageh205@gmail.com>
2024-10-02 14:23:12 +0300

    feat: adding model and controller for category model with routes

---
+++ Backend/src/Controllers/category.controller.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+
+import { Category } from "../Database/Models/category.model";
+
+export const createCategory = async (req: Request, res: Response) => {
+	try {
+		const { name } = req.body;
+		if (!name) {
+			res.status(400).send("name is required");
+		}
+		const category = await Category.create({ name });
+		res.status(201).send(category);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error create an activity category");
+	}
+};
+
+export const getCategories = async (req: Request, res: Response) => {
+	try {
+		const categories = await Category.find();
+		res.status(200).send(categories);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error getting activity categories");
+	}
+};
+
+export const updateCategory = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		const { name } = req.body;
+		const category = await Category.findByIdAndUpdate(
+			id,
+			{ name },
+			{ new: true },
+		);
+		if (!category) {
+			return res.status(404).send("Cant find Activity");
+		}
+		res.status(200).send(category);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error updating activity category");
+	}
+};
+
+export const deleteCategory = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+		await Category.findByIdAndDelete(id);
+		res.status(200).send("Category deleted Succefully");
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Error deleting Category");
+	}
+};
