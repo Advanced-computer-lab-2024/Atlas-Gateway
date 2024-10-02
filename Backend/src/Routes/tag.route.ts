472b74f4eb29c0c003439b4142cdad5aeb4d869b
Omar_Riad <omarriad2006@gmail.com>
2024-10-02 21:20:35 +0300

    feat: tags-CRUD-22,24 -- Completed

--- Backend/src/Routes/tag.route.ts
+++ Backend/src/Routes/tag.route.ts
@@ -5,4 +5,11 @@ import * as tagController from "../Controllers/tag.controller";
 
 const tagRouter = Router();
 
+tagRouter.post("/historical/create", tagController.createHistTag);
+tagRouter.get("/historical/get", tagController.getHistTags);
+tagRouter.post("/preference/create", tagController.createPrefTag);
+tagRouter.get("/preference/get", tagController.getPrefTags);
+tagRouter.put("/preference/update/:id", tagController.updatePrefTag);
+tagRouter.delete("/preference/delete/:id", tagController.deletePreTag);
+
 export default tagRouter;
