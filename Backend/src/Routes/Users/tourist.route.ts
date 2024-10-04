d6b9974008f615453144b23e524d09dfc2bb95e0
Marawan Salah <Marawansalah002@hotmail.com>
2024-10-04 08:52:09 +0300

    Tourist Model/Router/Controller Creation and updates

---
+++ Backend/src/Routes/Users/tourist.route.ts
@@ -0,0 +1,16 @@
+import { Express, Router } from "express";
+
+import {
+	createTourist,
+	deleteTourist,
+	getTourists,
+	updateTourist,
+} from "../../Controllers/Users/tourist.controller";
+
+const touristRouter = Router();
+touristRouter.post("/create", createTourist);
+touristRouter.get("/list", getTourists);
+touristRouter.put("/update/:id", updateTourist);
+touristRouter.delete("/delete/:id", deleteTourist);
+export default touristRouter;
+Router;
