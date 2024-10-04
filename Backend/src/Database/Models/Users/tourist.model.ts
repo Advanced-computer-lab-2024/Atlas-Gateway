28663ec97162ef25e957b2976d67d6f2a295bb8e
Marawan Salah <Marawansalah002@hotmail.com>
2024-10-04 08:52:09 +0300

    Tourist Model/Router/Controller Creation and updates

--- Backend/src/Database/Models/Users/tourist.model.ts
+++ Backend/src/Database/Models/Users/tourist.model.ts
@@ -7,10 +7,13 @@ export interface ITourist extends Document {
 	email: string; // story 3
 	password: string;
 	wallet: number; // story 11
-	address: string; //  story 98 99
-	currency: string; // story 51
-	loyaltyPoints: number; // story 70
-	isDeleted: boolean;
+	mobileNumber: number;
+	nationality: string;
+	dob: Date;
+	job: string;
+	address?: string[]; //  story 98 99 Maro comment: Multiple addresses can be added
+	currency?: string; // story 51
+	loyaltyPoints?: number; // story 70
 	profile?: {
 		bio?: string;
 		location?: string;
@@ -25,10 +28,13 @@ const touristSchema = new Schema<ITourist>(
 		email: { type: String, required: true },
 		password: { type: String, required: true },
 		wallet: { type: Number, required: true },
-		address: { type: String, required: true },
-		currency: { type: String, required: true },
+		mobileNumber: { type: Number, required: true },
+		nationality:{type: String, required: true},
+		dob: { type: Date, required: true },
+		job: { type: String, required: true },
+		address: [{ type: String}],
+		currency: { type: String},
 		loyaltyPoints: { type: Number, required: true },
-		isDeleted: { type: Boolean },
 		profile: {
 			bio: { type: String },
 			location: { type: String },
