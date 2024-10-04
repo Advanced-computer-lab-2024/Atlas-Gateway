3b1728eb2b3bb094947b96108c6fb2e3d84a949c
Mohamad Rageh <rageh205@gmail.com>
2024-10-04 12:05:58 +0300

    feat: added function to return array of categorys

--- Frontend/src/Admin/utils/getObjectIdsValues.ts
+++ Frontend/src/Admin/utils/getObjectIdsValues.ts
@@ -1,1 +1,14 @@
-const
+import axios from "axios";
+
+const getObjectIdsValues = async (array: string[]): Promise<string[]> => {
+	const result: string[] = [];
+	try {
+		for (const elem of array) {
+			const res = await axios.get("");
+			result.push(res.data);
+		}
+	} catch (error) {
+		console.log(error);
+	}
+	return result;
+};
