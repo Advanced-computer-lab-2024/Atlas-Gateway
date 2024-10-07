7c22a46b74fa6e9517f6308b8c4f20b27e629f68
Mohamad Rageh <rageh205@gmail.com>
2024-10-07 15:26:34 +0300

    button to navigate back to homepage

--- Frontend/src/Admin/Sidebar.tsx
+++ Frontend/src/Admin/Sidebar.tsx
@@ -6,12 +6,16 @@ import {
 	Users,
 } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { useLoginStore } from "@/store/loginStore";
 
 interface Props {
 	onSelect: (item: string) => void;
 }
 
 const Sidebar = ({ onSelect }: Props) => {
+	const { user } = useLoginStore();
 	const [activeItem, setActiveItem] = useState("");
 
 	const handleClick = (item: string) => {
@@ -21,14 +25,22 @@ const Sidebar = ({ onSelect }: Props) => {
 
 	return (
 		<div className="flex flex-col items-center w-60 h-full bg-[#2b58ad] text-center p-4 space-y-4 gap-3">
-			<div className="flex flex-col gap-3 items-center">
-				<UserCircleIcon
-					className="p-1 rounded-full text-white"
-					width={120}
-					height={120}
-				/>
+			<div className="flex flex-col gap-4 items-center">
+				<div>
+					<Link
+						to={"/"}
+						className="text-primary hover:no-underline hover:text-primary text-lg text-white font-[pacifico]"
+					>
+						AtlasGateway
+					</Link>
+					<UserCircleIcon
+						className="p-1 rounded-full text-white"
+						width={120}
+						height={120}
+					/>
+				</div>
 				<h3 className="text-[whitesmoke] text-lg font-semibold">
-					Name
+					{user?.username}
 				</h3>
 				<button className="bg-[#2b58ad] text-white py-2 px-4 rounded-lg border-white">
 					Update Profile
