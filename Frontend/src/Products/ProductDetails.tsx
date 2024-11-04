3654ca2182b5b12afe76700995aa1e989ec368d3
Omar Riad <126183421+RiadOmar@users.noreply.github.com>
2024-11-04 22:05:00 +0200

    feat: rating functionality (product)

--- Frontend/src/Products/ProductDetails.tsx
+++ Frontend/src/Products/ProductDetails.tsx
@@ -1,17 +1,48 @@
-import { ArrowLeft, DollarSign, LocateIcon, Star } from "lucide-react";
+import axios from "axios";
+import { set } from "lodash";
+import { ArrowLeft, DollarSign, LocateIcon } from "lucide-react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useProduct } from "@/api/data/useProducts";
 import Label from "@/components/ui/Label";
 import { Card } from "@/components/ui/card";
 import { Flex } from "@/components/ui/flex";
-import Rating from "@/components/ui/rating";
+import Rating, { ratingType } from "@/components/ui/rating";
 
 export default function ProductDetails() {
 	const navigate = useNavigate();
 	const { data } = useProduct();
 	const { name, description, images, price, avgRating, availability } =
 		data || {};
+	const [displayValue, setDisplayValue] = React.useState<number | undefined>(
+		0,
+	);
+
+	useEffect(() => {
+		setDisplayValue(avgRating || 0);
+	}, [avgRating]);
+
+	const handleRateingChange = async (inputRating: number) => {
+		try {
+			await axios.post("http://localhost:5000/api/products/addRating", {
+				productId: data?._id,
+				rating: inputRating,
+				//User ID will be included in the request header
+			}); //TODO: Add the correct endpoint when it is available
+			//Show "Rating Saved" Prompt in frontend
+		} catch (error) {
+			console.error(error);
+		}
+		const confirmText = document.getElementById("confirmation");
+		if (confirmText) {
+			confirmText.classList.remove("hidden");
+			setDisplayValue(inputRating);
+			setTimeout(() => {
+				confirmText.classList.add("hidden");
+			}, 3000);
+		}
+	};
 
 	return (
 		<Flex
@@ -72,7 +103,20 @@ export default function ProductDetails() {
 								<Label.Mid500 className="overflow-ellipsis">
 									{avgRating}
 								</Label.Mid500> */}
-								<Rating value={avgRating} />
+								<Flex className="flex-col items-start">
+									<Rating
+										value={displayValue}
+										ratingType={ratingType.DETAILS}
+										interactable={true}
+										onChange={handleRateingChange}
+									/>
+									<p
+										id="confirmation"
+										className="flex-basis-full mt-0 ml-60 text-left hidden z-10 absolute bg-green-500 text-white p-2 rounded-lg"
+									>
+										Saved
+									</p>
+								</Flex>
 							</Flex>
 						</Flex>
 					</Flex>
--- Frontend/src/components/ui/rating.tsx
+++ Frontend/src/components/ui/rating.tsx
@@ -1,18 +1,59 @@
-import { transform } from "lodash";
-import { Star, StarHalf } from "lucide-react";
+import { Star } from "lucide-react";
 import React from "react";
 
-let starSize = 40;
+export enum ratingType {
+	DETAILS = 40,
+	CARDS = 10,
+}
 
 interface RatingProps {
 	value: number;
-	onchange?: (value: number) => void | null;
+	onChange?: (value: number) => void | null;
+	ratingType?: ratingType;
+	interactable?: boolean;
 }
 
-const Rating: React.FC<RatingProps> = ({ value }) => {
+const Rating: React.FC<RatingProps> = ({
+	value,
+	ratingType = ratingType.CARDS,
+	interactable = false,
+	onChange,
+}) => {
+	const [hoverValue, setHoverValue] = React.useState<number | null>(null);
+	const displayValue: number = hoverValue !== null ? hoverValue : value;
+	const hoverClass = "hover:scale-125";
+	const handleMouseEnter = (index: number, event: React.MouseEvent) => {
+		if (interactable) {
+			setHoverValue(index + 1);
+
+			event.currentTarget.classList.add(hoverClass);
+		}
+	};
+
+	const handleMouseLeave = (event: React.MouseEvent) => {
+		if (interactable) {
+			event.currentTarget.classList.remove(hoverClass);
+		}
+	};
+
+	const handleClick = (index: number, event: React.MouseEvent) => {
+		if (interactable) {
+			onChange(index + 1);
+			//send rating request to server
+			//Show "Rating Saved" Prompt in frontend
+		}
+	};
+
+	const rateLeave = () => {
+		if (interactable) {
+			setHoverValue(null);
+		}
+	};
+
+	const starSize = ratingType?.valueOf();
 	return (
-		<div className="rating flex">
-			{Array.from({ length: value }, (_, i) => (
+		<div className="rating flex" onMouseLeave={() => rateLeave()}>
+			{Array.from({ length: displayValue }, (_, i) => (
 				<Star
 					className="mx-1"
 					key={i}
@@ -20,16 +61,27 @@ const Rating: React.FC<RatingProps> = ({ value }) => {
 					strokeWidth={1}
 					fill="yellow"
 					size={starSize}
+					onMouseEnter={(e) => handleMouseEnter(i, e)}
+					onMouseLeave={(e) => handleMouseLeave(e)}
+					onClick={(e) => handleClick(i, e)}
 				/>
 			))}
 
-			{value % 1 !== 0 ? (
+			{displayValue % 1 !== 0 ? (
 				<div
 					className="relative mx-1"
+					key={Math.floor(displayValue)}
 					style={{ width: starSize, height: starSize }}
+					onMouseEnter={(e) =>
+						handleMouseEnter(Math.floor(displayValue), e)
+					}
+					onMouseLeave={(e) => handleMouseLeave(e)}
+					onClick={(e: React.MouseEvent<Element, MouseEvent>) =>
+						handleClick(Math.floor(displayValue), e)
+					}
 				>
 					<Star
-						className="absolute top-0 left-0 z-10"
+						className="absolute top-0 left-0 z-10 "
 						color="yellow"
 						strokeWidth={1}
 						fill="yellow"
@@ -51,14 +103,17 @@ const Rating: React.FC<RatingProps> = ({ value }) => {
 				</div>
 			) : null}
 
-			{Array.from({ length: 5 - value }, (_, i) => (
+			{Array.from({ length: 5 - displayValue }, (_, i) => (
 				<Star
 					className="mx-1"
-					key={value + i}
+					key={displayValue + i}
 					color="gray"
 					strokeWidth={1}
 					fill="white"
 					size={starSize}
+					onMouseEnter={(e) => handleMouseEnter(displayValue + i, e)}
+					onMouseLeave={(e) => handleMouseLeave(e)}
+					onClick={(e) => handleClick(displayValue + i, e)}
 				/>
 			))}
 		</div>
