e742476c6a908c7f5482edaacdd79310bf285eb5
Mohamad Rageh <rageh205@gmail.com>
2024-11-06 23:37:14 +0200

    fix: updating import path

--- Backend/src/Controllers/Travel/itinerary.controller.ts
+++ Backend/src/Controllers/Travel/itinerary.controller.ts
@@ -5,10 +5,10 @@ import HttpError from "../../Errors/HttpError";
 import { Itinerary } from "../../Models/Travel/itinerary.model";
 import { Tourist } from "../../Models/Users/tourist.model";
 import * as itineraryService from "../../Services/Travel/itinerary.service";
-import { Types } from "mongoose";
-import { Itinerary } from "@/Models/Travel/itinerary.model";
-import { Tourist } from "@/Models/Users/tourist.model";
-import { addBookedItinerary, cancelItinerary } from "@/Services/Users/tourist.service";
+import {
+	addBookedItinerary,
+	cancelItinerary,
+} from "../../Services/Users/tourist.service";
 
 export const createItinerary = async (
 	req: Request,
@@ -165,7 +165,9 @@ export const bookItinerary = async (req: Request, res: Response) => {
 		}
 
 		if (!itineraryId) {
-			return res.status(400).json({ message: "Itinerary ID is required" });
+			return res
+				.status(400)
+				.json({ message: "Itinerary ID is required" });
 		}
 
 		if (!Types.ObjectId.isValid(itineraryId)) {
@@ -192,7 +194,9 @@ export const bookItinerary = async (req: Request, res: Response) => {
 			return res.status(400).json({ message: "Cannot book Itinerary" });
 		}
 
-		return res.status(201).json({ message: "Itinerary booked successfully" });
+		return res
+			.status(201)
+			.json({ message: "Itinerary booked successfully" });
 	} catch (error) {
 		return res.status(500).json({ message: "Error booking Itinerary" });
 	}
@@ -208,7 +212,9 @@ export const cancelBookingItinerary = async (req: Request, res: Response) => {
 		}
 
 		if (!itineraryId) {
-			return res.status(400).json({ message: "Itinerary ID is required" });
+			return res
+				.status(400)
+				.json({ message: "Itinerary ID is required" });
 		}
 
 		if (!Types.ObjectId.isValid(itineraryId)) {
@@ -227,7 +233,10 @@ export const cancelBookingItinerary = async (req: Request, res: Response) => {
 			return res.status(404).json({ message: "Tourist not found" });
 		}
 
-		const cancelBookingResult = cancelBookingItinerary(itinerary.id, tourist.id);
+		const cancelBookingResult = cancelBookingItinerary(
+			itinerary.id,
+			tourist.id,
+		);
 
 		const removeBookingResult = cancelItinerary(tourist.id, itinerary.id);
 
@@ -235,7 +244,9 @@ export const cancelBookingItinerary = async (req: Request, res: Response) => {
 			return res.status(400).json({ message: "Cannot book Itinerary" });
 		}
 
-		return res.status(201).json({ message: "Itinerary booked successfully" });
+		return res
+			.status(201)
+			.json({ message: "Itinerary booked successfully" });
 	} catch (error) {
 		return res.status(500).json({ message: "Error booking Itinerary" });
 	}
