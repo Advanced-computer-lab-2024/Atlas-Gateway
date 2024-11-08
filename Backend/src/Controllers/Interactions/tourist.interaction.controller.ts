f70326fcf85977e55531e6f613c3216efcf69079
DrBlende <omarf5925@gmail.com>
2024-11-08 14:38:40 +0200

    shsh

--- Backend/src/Controllers/Interactions/tourist.interaction.controller.ts
+++ Backend/src/Controllers/Interactions/tourist.interaction.controller.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response, response } from "express";
 import { Types } from "mongoose";
 
+import { ActivityComment } from "../../Models/Interactions/Comments/Activity.Comment";
 import { ItineraryComment } from "../../Models/Interactions/Comments/Itinerary.Comment.Model";
 import { TourGuideComment } from "../../Models/Interactions/Comments/TourGuide.Comment.Model";
+import { ActivityRate } from "../../Models/Interactions/Rates/Activity.Rate.Model";
 import { ItineraryRate } from "../../Models/Interactions/Rates/Itinerary.Rate.model";
 import { TourGuideRate } from "../../Models/Interactions/Rates/TourGuide.Rate.model";
 import { Product } from "../../Models/Purchases/product.model";
+import { Activity } from "../../Models/Travel/activity.model";
 import { Itinerary } from "../../Models/Travel/itinerary.model";
 import { TourGuide } from "../../Models/Users/tourGuide.model";
 import { Tourist } from "../../Models/Users/tourist.model";
@@ -25,7 +28,7 @@ export const rateTourGuide = async (req: Request, res: Response) => {
 		}
 		const oldAverage = tourGuide.avgRating;
 		let oldCount = tourGuide.totalNumberOfRatings;
-		const itineraries = tourist.bookedItinerary;
+		const itineraries = tourist.bookedItineraries;
 		const currentDate = new Date();
 		let interaction;
 		let newAverage;
@@ -48,9 +51,14 @@ export const rateTourGuide = async (req: Request, res: Response) => {
 				if (interaction.isNew) {
 					newAverage =
 						(oldAverage * oldCount + value) / (oldCount + 1);
+					console.log(newAverage);
 					oldCount = oldCount + 1;
+					console.log(oldCount);
 				} else {
-					newAverage = oldAverage * (oldCount - 1) + value / oldCount;
+					newAverage =
+						(oldAverage * (oldCount - 1) + value) / oldCount;
+					console.log(newAverage);
+					console.log(oldCount);
 				}
 			}
 			await TourGuide.findByIdAndUpdate(
@@ -75,7 +83,7 @@ export const commentTourGuide = async (req: Request, res: Response) => {
 			res.status(400).send("tourist not found");
 			return;
 		}
-		const itineraries = tourist.bookedItinerary;
+		const itineraries = tourist.bookedItineraries;
 		const currentDate = new Date();
 		let interaction;
 
@@ -174,7 +182,7 @@ export const rateItinerary = async (req: Request, res: Response) => {
 		}
 		const oldAverage = itinerary.avgRating;
 		let oldCount = itinerary.totalNumberOfRatings;
-		const itineraries = tourist.bookedItinerary;
+		const itineraries = tourist.bookedItineraries;
 		const currentDate = new Date();
 		let interaction;
 		let newAverage;
@@ -195,7 +203,8 @@ export const rateItinerary = async (req: Request, res: Response) => {
 						(oldAverage * oldCount + value) / (oldCount + 1);
 					oldCount = oldCount + 1;
 				} else {
-					newAverage = oldAverage * (oldCount - 1) + value / oldCount;
+					newAverage =
+						(oldAverage * (oldCount - 1) + value) / oldCount;
 				}
 				await Itinerary.findByIdAndUpdate(
 					itineraryId,
@@ -214,36 +223,9 @@ export const rateItinerary = async (req: Request, res: Response) => {
 			console.log(itineraries);
 			console.log(itineraryId);
 		}
-
-		// for (let i = 0; i < itineraries.length; i++) {
-		// 	const itinerary = await Itinerary.findById(itineraries[i]);
-		// 	if (!itinerary) {
-		// 		res.status(400).send("Itinerary not found booked by tourist");
-		// 		return;
-		// 	}
-		// 	if (
-		// 		itinerary.id == itineraryId &&
-		// 		itinerary.endDateTime < currentDate
-		// 	) {
-		// 		interaction = await ItineraryRate.findOneAndUpdate(
-		// 			{ touristId, itineraryId },
-		// 			{ $set: { value } },
-		// 			{ upsert: true, new: true },
-		// 		);
-		// 		console.log(interaction);
-		// 		console.log("hello");
-		// 		if (interaction.isNew) {
-		// 			newAverage =
-		// 				(oldAverage * oldCount + value) / (oldCount + 1);
-		// 			oldCount = oldCount + 1;
-		// 		} else {
-		// 			newAverage = oldAverage * (oldCount - 1) + value / oldCount;
-		// 		}
-		// 	} else {
-		// 		res.status(400).send("Event hasnt started yet");
-		// 	}
 	} catch (error) {
 		res.status(500).send(error);
+		console.log(error);
 	}
 };
 export const commentItinerary = async (req: Request, res: Response) => {
@@ -258,7 +240,7 @@ export const commentItinerary = async (req: Request, res: Response) => {
 		if (!itinerary) {
 			res.status(400).send("Event not found");
 		}
-		const itineraries = tourist.bookedItinerary;
+		const itineraries = tourist.bookedItineraries;
 		const currentDate = new Date();
 		let interaction;
 
@@ -278,6 +260,98 @@ export const commentItinerary = async (req: Request, res: Response) => {
 					text,
 				});
 			}
+		}
+		res.status(201).send(interaction);
+	} catch (error) {
+		res.status(400).send(error);
+	}
+};
+export const rateActivity = async (req: Request, res: Response) => {
+	try {
+		const { touristId, activityId, value } = req.body;
+		const tourist = await Tourist.findById(touristId);
+		const activity = await activityId.findById(activityId);
+		if (!tourist) {
+			res.status(404).send("tourist not found");
+			return;
+		}
+		if (!activity) {
+			res.status(404).send("activity not found");
+			return;
+		}
+		const oldAverage = activity.avgRating;
+		let oldCount = activity.totalNumberOfRatings;
+		const activities = tourist.bookedActivities;
+		const currentDate = new Date();
+		let interaction;
+		let newAverage;
+		if (activities.includes(activityId)) {
+			const activity = await Activity.findById(activityId);
+			if (!activity) {
+				res.status(400).send("the itinerary passed isnt found");
+				return;
+			}
+			if (activity.dateTime < currentDate) {
+				interaction = await ActivityRate.findOneAndUpdate(
+					{ touristId, activityId },
+					{ $set: { value } },
+					{ upsert: true, new: true },
+				);
+				if (interaction.isNew) {
+					newAverage =
+						(oldAverage * oldCount + value) / (oldCount + 1);
+					oldCount = oldCount + 1;
+				} else {
+					newAverage =
+						(oldAverage * (oldCount - 1) + value) / oldCount;
+				}
+				await Activity.findByIdAndUpdate(
+					activityId,
+					{
+						totalNumberOfRatings: oldCount,
+						avgRating: newAverage,
+					},
+					{ new: true },
+				);
+				res.status(201).send(interaction);
+			} else {
+				res.status(400).send("Event hasnt started yet");
+			}
+		} else {
+			res.status(400).send("Event isnt booked by Tourist");
+		}
+	} catch (error) {
+		res.status(500).send("error");
+	}
+};
+export const commentActivity = async (req: Request, res: Response) => {
+	try {
+		const { touristId, activityId, text } = req.body;
+		const tourist = await Tourist.findById(touristId);
+		const activity = await Activity.findById(activityId);
+		if (!tourist) {
+			res.status(400).send("tourist not found");
+			return;
+		}
+		if (!activity) {
+			res.status(400).send("Event not found");
+			return;
+		}
+		const activities = tourist.bookedActivities;
+		const currentDate = new Date();
+		let interaction;
+		if (activities.includes(activityId)) {
+			if (activity.dateTime < currentDate) {
+				interaction = await ActivityComment.create({
+					touristId,
+					activityId,
+					text,
+				});
+			} else {
+				res.status(400).send("activity date hasnt come yet");
+			}
+		} else {
+			res.status(400).send("activity isnt booked by user");
 		}
 		res.status(201).send(interaction);
 	} catch (error) {
