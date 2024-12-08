02f6e932d241cf20aa527f944106e51e0dab0a3f
Nasser <youssef.nasser.ashour@gmail.com>
2024-12-08 18:32:04 +0200

    Fix:get all notifs (#117)

--- Frontend/src/api/data/useNotifications.ts
+++ Frontend/src/api/data/useNotifications.ts
@@ -1,50 +1,45 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 import { useLoginStore } from "@/store/loginStore";
-
-import { apiGetNotifications, apiMarkNotificationAsRead, apiDeleteNotification } from "../service/notifications";
-import { useQueryString } from "./useQueryString";
-import { useParams } from "react-router-dom";
-import { TApiResponse } from "../service/types";
 import { TNotification } from "@/types/global";
 
+import {
+	apiDeleteNotification,
+	apiGetNotifications,
+	apiMarkNotificationAsRead,
+} from "../service/notifications";
+
 export function useGetNotifications() {
-  const { id } = useParams<{ id: string }>();
+	const { user } = useLoginStore();
+	const { _id } = user || {};
 
-  const { data, isLoading, error } = useQuery<TApiResponse<TNotification[]>, Error>({
-    queryFn: () => {
-      if (!id) {
-        return Promise.reject("User ID is required");
-      }
-      return apiGetNotifications(id); // This should return a TApiResponse<TNotification[]>
-    },
-    queryKey: ["notification", id],
-    enabled: !!id, // Ensures the query is only run if id exists
-  });
+	const q = useQuery({
+		queryFn: () => apiGetNotifications(_id),
+		queryKey: ["notifications", _id],
+	});
 
-  return {
-    data: data?.data || [], // Access the correct 'data' field from the response
-    isLoading,
-    error,
-  };
+	const { data } = q;
+
+	return { ...q, data: data?.data };
 }
 
 export function useMarkNotificationAsRead(onSuccess: () => void) {
-  const mutation = useMutation({
-    mutationFn: (id: string) => apiMarkNotificationAsRead(id),
-    onSuccess,
-  });
+	const mutation = useMutation({
+		mutationFn: (id: string) => apiMarkNotificationAsRead(id),
+		onSuccess,
+	});
 
-  const { mutate } = mutation;
-  return { markAsRead: mutate, ...mutation };
+	const { mutate } = mutation;
+	return { markAsRead: mutate, ...mutation };
 }
 
 export function useDeleteNotification(onSuccess: () => void) {
-  const mutation = useMutation({
-    mutationFn: (id: string) => apiDeleteNotification(id),
-    onSuccess,
-  });
+	const mutation = useMutation({
+		mutationFn: (id: string) => apiDeleteNotification(id),
+		onSuccess,
+	});
 
-  const { mutate } = mutation;
-  return { deleteNotification: mutate, ...mutation };
+	const { mutate } = mutation;
+	return { deleteNotification: mutate, ...mutation };
 }
