751e9608db20c14397a14ca347766ba935851daa
Mohamad Rageh <rageh205@gmail.com>
2024-12-09 05:49:32 +0200

    random images for hotels

--- Frontend/src/Hotels/HotelsCard.tsx
+++ Frontend/src/Hotels/HotelsCard.tsx
@@ -11,6 +11,12 @@ import {
 import { Flex } from "@/components/ui/flex";
 import { THotel } from "@/types/global";
 
+import hotel1 from "../assets/hotel1.jpeg";
+import hotel2 from "../assets/hotel2.jpeg";
+import hotel3 from "../assets/hotel3.jpg";
+import hotel4 from "../assets/hotel4.jpeg";
+import hotel5 from "../assets/hotel5.jpeg";
+
 const HotelsCard = ({
 	hotel,
 	openEditDrawer,
@@ -18,6 +24,9 @@ const HotelsCard = ({
 	hotel: THotel;
 	openEditDrawer: (hotel: THotel) => void;
 }) => {
+	const images = [hotel1, hotel2, hotel3, hotel4, hotel5];
+	const randomImage = Math.floor(Math.random() * 5);
+
 	return (
 		<Card className="w-full h-[280px] flex gap-2 flex-col border-black border-2 font-bold text-xl">
 			<CardHeader>
@@ -41,8 +50,13 @@ const HotelsCard = ({
 					</DropdownMenu>
 				</Flex>
 			</CardHeader>
+			<img
+				src={images[randomImage]}
+				alt={`${hotel.name} image`}
+				className="w-full h-[100px] object-cover"
+			/>
 			<CardContent>
-				<Flex isColumn gap="3">
+				<Flex gap="3" justify="between">
 					<Flex isColumn gap="2">
 						<Label.Mid300>Chain code</Label.Mid300>
 						<Label.Mid500>{hotel.chainCode}</Label.Mid500>
