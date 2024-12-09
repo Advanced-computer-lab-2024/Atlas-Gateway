72895d6608ef6a04a76dbe9d7e2d01ed058b905c
DottierDark <ashmxwy@gmail.com>
2024-12-09 02:03:57 +0200

    hotfix

--- Frontend/src/Profile/Tourist/TouristProfile.tsx
+++ Frontend/src/Profile/Tourist/TouristProfile.tsx
@@ -43,7 +43,7 @@ export default function TouristProfile() {
 								Account Details
 							</TabsTrigger>
 							<TabsTrigger value="transportations">
-								Transporations & Hotels
+								Transportations & Hotels
 							</TabsTrigger>
 							<TabsTrigger value="upcoming">
 								Upcoming Bookings
